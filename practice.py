b2c2969c1c41cb8b43e4ec42eadb4ae9e6b5128f
mastanreddy <masthan.py@gmail.com>
2020-07-21 12:10:50 +0530

    add new file

---
+++ practice.py
@@ -0,0 +1,30 @@
+'''a=int(input('enter your value:'))
+if a%2==0:
+     print('the given value is even')
+else:
+     print('the given value is odd')'''
+
+#grater value find
+'''a=int(input('enter first value:'))
+b=int(input('enter second value:'))
+if a>b:
+    print(a,'a is grater value:')
+else:
+    print('the value is low:')'''
+
+'''a=int(input('enter first value:'))
+b=int(input('enter second value:'))
+c=int(input('enter third value:'))
+if a>b and a>c:
+    print(a, 'is grater value:')
+elif b>c:
+     print(b,'is grater value:')
+
+else:
+    print(c,'is grater value')
+    print('thank you')''''
+
+       
+     
+            
+      
